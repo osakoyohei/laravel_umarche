1d064117f9406c39c1924a09f90c2735df938fc7
osakoyohei <sako4.nsr@gmail.com>
2021-09-10 00:53:50 +0900

    画像アップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 class ShopController extends Controller
 {
@@ -29,8 +33,7 @@ class ShopController extends Controller
 
     public function index()
     {
-        $ownerId = Auth::id();
-        $shops = Shop::where('owner_id', $ownerId)->get();
+        $shops = Shop::where('owner_id', Auth::id())->get();
 
         return view('owner.shops.index',
         compact('shops'));
@@ -38,11 +41,34 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        
+        $shop = Shop::findOrFail($id);
+
+        return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'information' => 'required|string|max:1000',
+            'is_selling' => 'required',
+        ]);
 
+        $imageFile = $request->image;
+        if(!is_null($imageFile) && $imageFile->isValid()){
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
+        }
+
+        $shop = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->information = $request->information;
+        $shop->is_selling = $request->is_selling;
+        if(!is_null($imageFile) && $imageFile->isValid()){
+            $shop->filename = $fileNameToStore;
+        }
+
+        $shop->save();
+
+        return redirect()->route('owner.shops.index')->with(['message' => '店舗情報を更新しました。', 'status' => 'info']);
     }
 }
