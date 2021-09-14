e8e399049ad68c4e893a68f64ecc7ef2119fee6b
大迫　洋平 <77446081+osakoyohei@users.noreply.github.com>
2021-09-14 14:01:22 +0900

    Revert "Feature/micromodal"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
         <div class="min-h-screen bg-gray-100">
             @if(auth('admin')->user())
                 @include('layouts.admin-navigation')
-            @elseif(auth('owners')->user())
+            @elseif(auth('owner')->user())
                 @include('layouts.owner-navigation')
             @elseif(auth('users')->user())
                 @include('layouts.user-navigation')
