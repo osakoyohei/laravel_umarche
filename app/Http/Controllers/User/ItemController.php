1d716debd11dbb8712f23038fe19e0edf9ddd58f
osakoyohei <sako4.nsr@gmail.com>
2021-09-18 02:28:58 +0900

    メール設定実装、README.md編集

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
-use Illuminate\Support\Facades\DB;
 use App\Models\PrimaryCategory;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -28,7 +31,7 @@ class ItemController extends Controller
     }
 
     public function index(Request $request)
-    {
+    {        
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
