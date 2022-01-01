6b0d159bc79125655f5afbc81ed04c85f281f908
osakoyohei <sako4.nsr@gmail.com>
2022-01-02 00:22:27 +0900

    factory編集

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -22,7 +22,7 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'name' => $this->faker->company,
             'information' => $this->faker->realText,
             'price' => $this->faker->numberBetween(10, 100000),
             'is_selling' => $this->faker->numberBetween(0,1),
